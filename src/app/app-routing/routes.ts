df6ab567c67d5eba484c45eb9e004948d60f3f85
talhanousher <talhanousher@gmail.com>
2018-10-28 01:04:19 +0500

    update home component and add promotion service

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { MenuComponent } from '../menu/menu.component';
+import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes = [
     {
         path: 'home', component: HomeComponent
     },
     {
         path: 'menu', component: MenuComponent
+    },
+    {
+        path: 'contactus', component: ContactComponent
     },
     {
         path: '', redirectTo: '/home', pathMatch: 'full'
