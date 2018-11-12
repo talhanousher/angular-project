0415675c77a68cf831e8cd8b8227e0a30a574b1c
talhanousher <talhanousher@gmail.com>
2018-11-12 22:37:26 +0500

    Validators on cantact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
 
@@ -10,6 +10,7 @@ import { Feedback, ContactType } from '../shared/feedback';
 export class ContactComponent implements OnInit {
   feedbackForm: FormGroup;
   feedback: Feedback;
+  @ViewChild('fform') feedBackFormDirective;
   contactType = ContactType;
   constructor(private fb: FormBuilder) {
     this.createForm();
@@ -19,10 +20,10 @@ export class ContactComponent implements OnInit {
   }
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      telnum: [0, Validators.required],
+      email: ['', Validators.required],
       agree: false,
       contacttype: 'None',
       message: ''
@@ -31,7 +32,16 @@ export class ContactComponent implements OnInit {
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: '',
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+    this.feedBackFormDirective.reset();
   }
 
 }
