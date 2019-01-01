a030ab7aa0d675abba788cf7ae0aef6a99750bb2
talhanousher <talhanousher@gmail.com>
2019-01-01 21:30:45 +0500

    adding animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,21 @@ import { DishService } from '../services/dish.service';
 import { PromotionService } from '../services/promotion.service';
 import { LeaderService } from '../services/leader.service';
 import { Leader } from '../shared/leader';
+import { flyInOut, expand } from '../animations/app.animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class HomeComponent implements OnInit {
   dish: Dish;
